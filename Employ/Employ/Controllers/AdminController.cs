b331d41c40a775a8f987e1e4b588183341ed6c87
CAF01 <Christian_pria@hotmail.com>
2022-12-03 21:50:04 -0600

    adding Basic structure

---
+++ Employ/Employ/Controllers/AdminController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Employ.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAlgo() { 
+            return new OkObjectResult(new List<int> { 1, 2, 3, 4, 5 });
+        }
+    }
+}
